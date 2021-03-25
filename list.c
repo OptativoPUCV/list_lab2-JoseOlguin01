31e91c40048ed442fba75d82ec977960fb470f66
Replit user <replituser@example.com>
2021-03-25 00:47:38 +0000

    new try

--- list.c
+++ list.c
@@ -45,7 +45,6 @@ void * firstList(List * list) {
 void * nextList(List * list) { 
     list->current=list->current->next;
     if(list->current==NULL){
-      list->current=list->current->next;
       return NULL;
     }else{
       return (void*) list->current->data;
