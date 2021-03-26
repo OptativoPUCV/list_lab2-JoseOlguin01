2378fe01c0aaaf1fd0b3c85067a6147280e6d6b2
Replit user <replituser@example.com>
2021-03-26 23:32:44 +0000

    new try

--- list.c
+++ list.c
@@ -88,7 +88,7 @@ void pushBack(List * list,  const void * data) {
 void pushCurrent(List * list, const void * data) {
 Node* a=createNode( (void*) data);
 
-a->next=list->current->next->next;
+a->next=list->current->prev;
 
 }
 
