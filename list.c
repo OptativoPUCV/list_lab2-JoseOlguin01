b819c4f74549e23eefdf29f2ff455c854f63ba98
Replit user <replituser@example.com>
2021-03-29 00:30:22 +0000

    new try

--- list.c
+++ list.c
@@ -90,7 +90,7 @@ Node* a=createNode( (void*) data);
     list->head=a;
     list->tail=a;
   }
-a->prev=list->head;
+a->next=list->head;
 }
 void * popFront(List * list) {
     list->current = list->head;
