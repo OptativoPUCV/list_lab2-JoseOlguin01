30fbccb75fd74a32f57e1a10d093ae534d9ead51
Replit user <replituser@example.com>
2021-03-29 20:42:05 +0000

    new try

--- list.c
+++ list.c
@@ -111,14 +111,14 @@ void * popBack(List * list) {
 void * popCurrent(List * list) {
     Node *x=list->current;
     void *a;
+    if(list->current==list->tail){
+      list->tail=x;
+    }
     list->current=x;
     a=list->current->data;
     if(list->current==NULL){
       return NULL;
     } 
-    if(list->current==list->tail){
-      list->tail=x;
-    }
     if(x->prev!=NULL){
       x->prev->next=x->next;
     }else{
